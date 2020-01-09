e59c84a7ccdb21076d1b925ed378cca7a6001b81
Dan James <danj@deeson.co.uk>
2020-01-09 22:51:30 +0000

    Allow technically valid null return

--- src/frontend/Routable.tsx
+++ src/frontend/Routable.tsx
@@ -3,6 +3,6 @@ import { ReactElement } from 'react';
 import { Api } from './api/interface';
 
 export type Routable<Params = {}, Props = {}> = {
-  (props: Params & RouteComponentProps): ReactElement;
+  (props: Params & RouteComponentProps): ReactElement | null;
   getInitialProps?(api: Api, params: Params | RouteComponentProps): Promise<Props>;
 };
